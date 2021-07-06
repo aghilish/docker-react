85cc9ee54fbd3622f8d8ff7766222f662e5fa3e6
Shahrooz Aghili <shahrooz33ce@gmail.com>
2021-07-06 18:04:26 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Some React App! </p>
+        <p>Some React App!! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
